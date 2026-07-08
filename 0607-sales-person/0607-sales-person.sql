78caaaee4edce7e5d8803e00c0cdb3c2ee0170d5
Gaurav-2111 <gauravdev339@gmail.com>
2026-07-08 18:20:37 +0530

    Time: 1373 ms (82.79%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,12 +1,6 @@
---  select s.name
---     from SalesPerson s
---         left join Orders o on s.sales_id = o.sales_id
---         left join Company c on o.com_id = c.com_id
---     where c.name = 'Red'
-
     select name
     from salesperson
-    where name not in ( select s.name
+    where sales_id not in ( select s.sales_id
     from SalesPerson s
         left join Orders o on s.sales_id = o.sales_id
         left join Company c on o.com_id = c.com_id
